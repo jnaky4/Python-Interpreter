96da268b8e74bab403245ace0e9a8f18160e750d
esb7cp <esb7cp@mail.missouri.edu>
2020-11-25 13:35:51 -0600

    started on the project

---
+++ Interpretator Missile/Interpretator Missile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Interpretator_Missile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Please use this interpeter to write python code. Use the command \"exit\" to quit the program.");
+            while (true)
+            {
+                string ourString = Console.ReadLine();
+                
+                if (ourString.Length > 3 && ourString.Substring(0, 4).CompareTo("exit") == 0) break;
+            }
+            
+        }
+    }
+}
