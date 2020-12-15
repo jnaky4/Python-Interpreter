1ce5e33c49648b76ff93f93f3ecc63297902135d
esb7cp <54416561+esb7cp@users.noreply.github.com>
2020-12-15 16:28:27 -0600

    Add files via upload

---
+++ Interpretator Missile/Interpretator Missile/Print.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    internal class Print
+    {
+        public static void PrintFunction(string ourString, List<(string, double)> numbers, List<(string, string)> strings)
+        {
+            string temp;
+            foreach(var s in numbers)
+            {
+                if (ourString.Contains(s.Item1))
+                {
+                    if (ourString.Contains("str("))
+                    {
+                        temp = ConvertToString(s.Item2);
+
+                        ourString = ourString.Replace("str(", "");
+                        ourString = ourString.Replace(s.Item1, temp);
+                        break;
+                    }
+                }
+            }
+            foreach (var s in strings)
+            {
+                if (ourString.Contains(s.Item1))
+                {
+                    temp = s.Item2;
+                    ourString = ourString.Replace(s.Item1, temp);
+                    break;
+                }
+            }
+            ourString = ourString.Replace("print(", "");
+            ourString = ourString.Replace("\"", "");
+            ourString = ourString.Replace("print(", "");
+            ourString = ourString.Replace(")", "");
+            ourString = ourString.Replace("+", "");
+            Console.WriteLine(ourString); 
+        }
+
+        public static string ConvertToString(double number)
+        {
+            return number.ToString();
+        }
+    }
+}
