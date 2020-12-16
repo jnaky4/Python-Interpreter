81821a8a6f4740497598a6ada1334badd97ebf3b
Levi Olson <60206657+klolson@users.noreply.github.com>
2020-12-16 17:59:20 -0600

    Fixed exception

--- Interpretator Missile/Interpretator Missile/Loops.cs
+++ Interpretator Missile/Interpretator Missile/Loops.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using static Interpretator_Missile.Logical_Statement;
 using System.Text;
 
 namespace Interpretator_Missile
@@ -8,10 +9,28 @@ namespace Interpretator_Missile
     {
 
         //while and for Loops 
-        //TODO
-        public static bool LOOP(string ourString, string key_word, int tabs)
+        ////1: Loop command: ie while, for, if    2: conditional logic    3: tabs
+        public static void LOOP(List<(string, double)> numbers, List<(string, string)> strings, List<(string, int)> block_commands, List<(string, string, int)> loop_stored)
         {
-            return true;
+            string keyword = loop_stored[loop_stored.Count - 1].Item1;
+            switch (keyword)
+            {
+                case ("if"):
+                    ifCondition(numbers, strings, block_commands, loop_stored);
+                    break;
+                default:
+                    break;
+            }
+            //return true;
+        }
+
+        public static void ifCondition(List<(string, double)> numbers, List<(string, string)> strings, List<(string, int)> block_commands, List<(string, string, int)> loop_stored)
+        {
+            string condition = loop_stored[loop_stored.Count - 1].Item2;
+            string keyword = loop_stored[loop_stored.Count - 1].Item1;
+            int tabs = loop_stored[loop_stored.Count - 1].Item3;
+            if (Logical_Statment(condition, keyword, tabs, numbers, strings) == true)
+                Console.WriteLine("Hooray");
         }
         
         public static string inLoop(string ourString, string[] keywords)
@@ -19,7 +38,7 @@ namespace Interpretator_Missile
             if (!(ourString[ourString.Length - 1] == ':'))
             {
                 Console.WriteLine("Error, missing :");
-                return null;
+                return "false";
             }
 
             foreach (var keyword in keywords)
