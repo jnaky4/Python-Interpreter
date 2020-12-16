ce00b97724b7d884cd9283bc584a4d2eba5712e0
esb7cp <esb7cp@mail.missouri.edu>
2020-12-16 17:36:57 -0600

    simple steps added math

--- Interpretator Missile/Interpretator Missile/Simple_Steps.cs
+++ Interpretator Missile/Interpretator Missile/Simple_Steps.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using org.mariuszgromada.math.mxparser;
 
 namespace Interpretator_Missile
 {
@@ -28,6 +29,44 @@ namespace Interpretator_Missile
             }
             return ourString;
             //**********************************
+        }
+        public static double Math(string ourString, List<(string, double)> numbers)
+        {
+
+            foreach(var s in numbers)
+            {
+                if (ourString.Contains(s.Item1))
+                {
+                    ourString = ourString.Replace(s.Item1, s.Item2.ToString());
+                }
+            }
+
+            Expression e = new Expression(ourString);
+            double x = e.calculate();
+
+            
+
+            if (double.IsNaN(x) == false)
+            {
+                if (ourString.Contains("<") || ourString.Contains("<=") || ourString.Contains(">") || ourString.Contains(">=") || ourString.Contains("==") || ourString.Contains("!="))
+                {
+                    if (x == 0) Console.WriteLine("False");
+                    else if (x == 1) Console.WriteLine("True");
+                    else Console.WriteLine(x.ToString());
+                }
+                else Console.WriteLine(x.ToString());
+            }
+            else if (ourString.Contains("%"))
+            {
+                ourString = ourString.Replace('%', '#');
+                Expression f = new Expression(ourString);
+                x = f.calculate();
+                if (double.IsNaN(x) == false)
+                {
+                    Console.WriteLine(x.ToString());
+                }
+            }
+            return x;
         }
 
     }
